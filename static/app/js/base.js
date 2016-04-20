63a4b569808bcd254ada857a1ede534e1344a2b2
侯宾宾 <kangyonggan@m2m.cn>
2016-04-20 11:31:26 +0800

    add perfect scroll bar

--- static/app/js/base.js
+++ static/app/js/base.js
@@ -7,9 +7,17 @@ var Util = Util || {};
 Util.windowHeight = function() {
     return window.innerHeight || document.documentElement.clientHeight;
 };
+Util.getValPX = function(val) {
+    var PX = 'px';
+    if(val.indexOf(PX) > -1) {
+        return val.split(PX)[0];
+    }
+    return val;
+};
 
 $(document).ready(function() {
     initContentHeight();
+    initScrollBar();
 });
 
 
@@ -18,4 +26,15 @@ function initContentHeight() {
     var headHeight = $('.site-header').height();
     var wh = Util.windowHeight();
     content.css('min-height', wh-headHeight);
+}
+
+function initScrollBar() {
+    //var headHeight = $('.site-header').height();
+    var footHeight = $('.site-footer').height();
+    var contentMinHeight = $('.page-content').css('min-height');
+    var pageContentWarp = $('.page-body-warp');
+    pageContentWarp.css('height', footHeight+Util.getValPX(contentMinHeight)).css('overflow-y', 'auto');
+    pageContentWarp.perfectScrollbar({
+        swipePropagation: false
+    });
 }
