623a93fbe93294a16946120e3a478c69e6b3d89c
侯宾宾 <kangyonggan@m2m.cn>
2016-04-20 09:16:19 +0800

    init the content height

--- static/app/js/base.js
+++ static/app/js/base.js
@@ -1,1 +1,21 @@
-console.log(120);
+/**
+ * !
+ * Created by houbb, All rights reserved.
+ * @type {Util|*|{}}
+ */
+var Util = Util || {};
+Util.windowHeight = function() {
+    return window.innerHeight || document.documentElement.clientHeight;
+};
+
+$(document).ready(function() {
+    initContentHeight();
+});
+
+
+function initContentHeight() {
+    var content = $('.page-content');
+    var headHeight = $('.site-header').height();
+    var wh = Util.windowHeight();
+    content.css('min-height', wh-headHeight);
+}
