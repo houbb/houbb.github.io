cf3dc416dda159f709adb51ed9f5f4463a07cb03
侯宾宾 <kangyonggan@m2m.cn>
2016-04-24 15:03:14 +0800

    auto read

--- static/app/js/base.js
+++ static/app/js/base.js
@@ -35,9 +35,48 @@ function Base(){
             theme:"light-thick"
         });
         $("#back-to-top").on('click', function() {
+            Base.autoScrollOff();
             $("body").mCustomScrollbar("scrollTo", "top");
         });
+        $('#auto-read-leaf').on('click', function() {
+            Base.autoReadLeaf();
+        });
     };
+    /**
+     * auto read;
+     */
+    var content=$("body"),autoScrollTimerAdjust,autoScroll;
+    this.autoReadLeaf = function() {
+        var speed = 20000;
+        var autoScrollTimer = (Util.windowHeight() / $('#mCSB_1_dragger_vertical').height()) * speed;
+        content.mCustomScrollbar({
+            scrollButtons:{enable:true},
+            callbacks:{
+                whileScrolling:function(){
+                    autoScrollTimerAdjust=autoScrollTimer*this.mcs.topPct/100;
+                },
+                onScroll:function(){
+                    if($(this).data("mCS").trigger==="internal"){Base.autoScrollOff()}
+                }
+            }
+        });
+        content.addClass("auto-scrolling-on auto-scrolling-to-bottom");
+        this.autoScrollOn("bottom",autoScrollTimer);
+    };
+    /**
+     *
+     * @param to
+     * @param timer
+     */
+    this.autoScrollOn = function(to,timer) {
+        content.addClass("auto-scrolling-on").mCustomScrollbar("scrollTo",to,{scrollInertia:timer,scrollEasing:"easeInOutSmooth"});
+    };
+    this.autoScrollOff = function() {
+        clearTimeout(autoScroll);
+        content.removeClass("auto-scrolling-on").mCustomScrollbar("stop");
+    };
+
+
 
     /**
      * select2 init
--- static/app/js/util.js
+++ static/app/js/util.js
@@ -13,6 +13,29 @@ var Util = Util || {};
 Util.windowHeight = function() {
     return window.innerHeight || document.documentElement.clientHeight;
 };
+
+Util.getInfo = function () {
+    var s = "";
+    s += " 网页可见区域宽："+ document.body.clientWidth;
+    s += " 网页可见区域高："+ document.body.clientHeight;
+    s += " 网页可见区域宽："+ document.body.offsetWidth + " (包括边线和滚动条的宽)";
+    s += " 网页可见区域高："+ document.body.offsetHeight + " (包括边线的宽)";
+    s += " 网页正文全文宽："+ document.body.scrollWidth;
+    s += " 网页正文全文高："+ document.body.scrollHeight;
+    s += " 网页被卷去的高(ff)："+ document.body.scrollTop;
+    s += " 网页被卷去的高(ie)："+ document.documentElement.scrollTop;
+    s += " 网页被卷去的左："+ document.body.scrollLeft;
+    s += " 网页正文部分上："+ window.screenTop;
+    s += " 网页正文部分左："+ window.screenLeft;
+    s += " 屏幕分辨率的高："+ window.screen.height;
+    s += " 屏幕分辨率的宽："+ window.screen.width;
+    s += " 屏幕可用工作区高度："+ window.screen.availHeight;
+    s += " 屏幕可用工作区宽度："+ window.screen.availWidth;
+    s += " 你的屏幕设置是 "+ window.screen.colorDepth +" 位彩色";
+    s += " 你的屏幕设置 "+ window.screen.deviceXDPI +" 像素/英寸";
+    return s;
+};
+
 /**
  * get the number value of width/height
  * @param val
@@ -57,3 +80,9 @@ Util.isMobile = function() {
 };
 
 
+/**
+ * cancel bubble
+ */
+Util.cancelBubble = function(event) {
+    if (event && event.stopPropagation) {event.stopPropagation(); } else { window.event.cancelBubble = true;}
+};
