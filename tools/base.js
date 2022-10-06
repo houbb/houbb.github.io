1f8f582fa1c8f5743e45fcec294ddb3c5c5815fe
binbin.hou <binbin.hou@huifu.com>
2022-10-06 18:43:49 +0800

    [Feature] add for new

--- tools/base.js
+++ tools/base.js
@@ -14,3 +14,38 @@
      document.execCommand('Copy'); // 执行浏览器复制命令
      oInput.remove()
  };
+
+function $(id) {
+    return document.getElementById(id);
+}
+
+// 日期格式化
+Date.prototype.formatDate = function (fmt) { 
+    "use strict"; 
+
+    var o = {
+        "M+": this.getMonth() + 1,                 //月份
+        "d+": this.getDate(),                    //日
+        "H+": this.getHours(),                   //小时
+        "m+": this.getMinutes(),                 //分
+        "s+": this.getSeconds(),                 //秒
+        "q+": Math.floor((this.getMonth() + 3) / 3), //季度
+        "S": this.getMilliseconds()             //毫秒
+    };
+    if (/(y+)/.test(fmt)) {
+        fmt = fmt.replace(RegExp.$1, (this.getFullYear() + '').substr(4 - RegExp.$1.length));
+    }
+    for (var k in o) {
+        if (new RegExp("(" + k + ")").test(fmt)) {
+            fmt = fmt.replace(RegExp.$1, (RegExp.$1.length === 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+        }
+    }
+    return fmt;
+} ;
+
+
+function parseDate(yyyyMMddHHmmss) {
+    let date = new Date(yyyyMMddHHmmss.replace('-', '/'));
+
+    return date;
+}
